529eda25c48c7bb0d986d73f7f62319754ba0081
okabekeigo <ito_kura@i.softbank.jp>
2024-12-18 14:47:42 +0900

    study:chapter3-1

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,28 +7,11 @@ use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function index(Request $request, Response $response){
-
-        $html = <<<EOF
-<html>
-<head>
-<title>Hello/Index</title>
-<style>
-body {font-size:16pt; color:#999; }
-h1 {font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px;}
-</style>
-</head>
-<body>
-    <h1>Index</h1>
-    <h3>Request</h3>
-    <pre>{$request}</pre>
-    <h3>Response</h3>
-    <pre>{$response}</pre>
-</body>
-</html>
-EOF;
-
-        $response->setContent($html);
-        return $response;
+    public function index(Request $request){
+        $data = [
+            'msg'=>'これはコントローラーから渡されたメッセージです。',
+            'id'=>$request->id
+        ];
+        return view('hello.index', $data);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('hello', function () {
-//     return '<html><body><h1>Hello</h1><p>Laravel開始！</p></body></html>'
-//     ;
-// });
-
 Route::get('/hello', [HelloController::class, 'index']);
