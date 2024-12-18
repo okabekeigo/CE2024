b1bc5f7a4880da1ba59012e0c1dd9b50b3621258
okabekeigo <ito_kura@i.softbank.jp>
2024-12-18 11:55:58 +0900

    feature:created first page

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hello', function () {
+    return '<html><body><h1>Hello</h1><p>Laravel開始！</p></body></html>';
+});
