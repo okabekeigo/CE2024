0632e71ffb372327c3c817d39b4b75661c43dc7b
Keigo Okabe <ito_kura@i.softbank.jp>
2025-01-04 17:57:20 +0900

    study:chapter4-3 in the middle

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,25 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Foundation\Validation\ValidatesRequests;
+use App\Http\Requests\HelloRequest;
 
 class HelloController extends Controller
 {
-    use ValidatesRequests;
 
     public function index()
     {
         return view('hello.index', ['msg' => 'フォームを入力：']);
     }
 
-    public function post(Request $request)
+    public function post(HelloRequest $request)
     {
-        $validate_rule = [
-            'name' => 'required',
-            'mail' => 'email',
-            'age' => 'numeric|between:0,150',
-        ];
-        $this->validate($request, $validate_rule);
         return view('hello.index', ['msg' => '正しく入力されました！']);
     }
 }
