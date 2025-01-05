798fc28fa8b3a56ead0b62417d67c4f75df2e52f
Keigo Okabe <ito_kura@i.softbank.jp>
2025-01-05 15:54:50 +0900

    study:chapter4-3 in the middle3

---
+++ app/Providers/HelloServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Validator;
+use  App\Http\Validators\HelloValidator;
+
+class HelloServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        $validator = $this->app['validator'];
+        $validator->resolver(function ($translator, $data, $rules, $messages) {
+            return new HelloValidator($translator, $data, $rules, $messages);
+        });
+    }
+}
