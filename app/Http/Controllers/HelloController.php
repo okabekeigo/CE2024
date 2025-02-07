8bd4b56c58882a136cba9d811b54e4a14b4d5d41
okabekeigo <ito_kura@i.softbank.jp>
2025-02-07 17:58:06 +0900

    study:chapter5-3 where

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -14,12 +14,7 @@ class HelloController extends Controller
 
     public function index(Request $request)
     {
-        if (isset($request->id)) {
-            $param = ['id' => $request->id];
-            $items = DB::select('select * from people where id = :id', $param);
-        } else {
-            $items = DB::select('select * from people');
-        }
+        $items = DB::table('people')->get();
 
         return view('hello.index', ['items' => $items]);
     }
@@ -88,5 +83,13 @@ class HelloController extends Controller
         ];
         DB::delete('delete from people where id = :id', $param);
         return redirect('/hello');
+    }
+
+
+    public function show(Request $request)
+    {
+        $id = $request->id;
+        $items = DB::table('people')->where('id', '<=', $id)->get();
+        return view('hello.show', ['items' => $items]);
     }
 }
