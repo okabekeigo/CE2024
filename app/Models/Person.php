09777b88b260114ff7432b2f0afe5f5b52b3033f
okabekeigo <ito_kura@i.softbank.jp>
2025-02-19 17:55:21 +0900

    study:chapter6-1

---
+++ app/Models/Person.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Person extends Model
+{
+    public function getData()
+    {
+        return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
+    }
+}
