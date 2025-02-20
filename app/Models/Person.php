ae37a6f599f5eb66c2e8d387370a46e80c180301
okabekeigo <ito_kura@i.softbank.jp>
2025-02-20 14:59:29 +0900

    study:chapter6-3

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -8,6 +8,14 @@ use Illuminate\Database\Eloquent\Builder;
 
 class Person extends Model
 {
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'name' => 'required',
+        'mail' => 'email',
+        'age' => 'integer|min:0|max:150',
+    );
+
     protected static function boot()
     {
         parent::boot();
