4c5e92149970144cf0f80d87fd0022472d11526c
okabekeigo <ito_kura@i.softbank.jp>
2025-02-21 15:47:45 +0900

    study:chapter7-3

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Requests\HelloRequest;
+use App\Models\Person;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
@@ -14,9 +15,10 @@ class HelloController extends Controller
 
     public function index(Request $request)
     {
-        $items = DB::table('people')->orderBy('age', 'asc')->get();
-
-        return view('hello.index', ['items' => $items]);
+        $sort = $request->sort;
+        $items = Person::orderBy($sort, 'asc')->paginate(3);
+        $param = ['items' => $items, 'sort' => $sort];
+        return view('hello.index', $param);
     }
 
     public function post(Request $request)
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,5 +1,31 @@
 @extends('layouts.helloapp')
 
+<style>
+    .pagination {
+        font-size: 10pt;
+    }
+
+    .pagination li {
+        display: inline-block
+    }
+
+    tr th a:link {
+        color: white
+    }
+
+    tr th a:visited {
+        color: white
+    }
+
+    tr th a:hover {
+        color: white
+    }
+
+    tr th a:active {
+        color: white
+    }
+</style>
+
 @section('title', 'Index')
 
 @section('menubar')
@@ -10,9 +36,9 @@
 @section('content')
     <table>
         <tr>
-            <th>Name</th>
-            <th>Mail</th>
-            <th>Age</th>
+            <th><a href="/hello?sort=name">name</a></th>
+            <th><a href="/hello?sort=mail">mail</a></th>
+            <th><a href="/hello?sort=age">age</a></th>
         </tr>
         @foreach ($items as $item)
             <tr>
@@ -22,6 +48,7 @@
             </tr>
         @endforeach
     </table>
+    {{ $items->appends(['sort' => $sort])->links() }}
 @endsection
 
 @section('footer')
